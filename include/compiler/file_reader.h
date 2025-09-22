8c2e44f1652b2a7d2786b91eadc721ef14b169f2
matee8 <graves-bluff-pesky@duck.com>
2025-09-23 01:19:11 +0200

    feat(file_reader): add file_reader module

---
+++ include/compiler/file_reader.h
@@ -0,0 +1,16 @@
+#ifndef COMPILER_FILE_READER_H
+#define COMPILER_FILE_READER_H
+
+#include <stdio.h>
+
+struct file_reader {
+    FILE* file;
+};
+
+int file_reader_init(struct file_reader* reader, const char* filename);
+int file_reader_read_all(struct file_reader* reader,
+                         char** content,
+                         size_t* len);
+void file_reader_destroy(struct file_reader* reader);
+
+#endif
