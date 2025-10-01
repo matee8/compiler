fa876bc5d6a65a3ce0e01520524ff2cb02ec4581
matee8 <graves-bluff-pesky@duck.com>
2025-10-02 00:13:32 +0200

    feat(string_buffer): implement sso-optimized string buffer module

---
+++ include/compiler/ds/string_buffer.h
@@ -0,0 +1,26 @@
+#ifndef COMPILER_DS_STRING_BUFFER_H
+#define COMPILER_DS_STRING_BUFFER_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+struct string_buffer {
+    union {
+        struct large_string {
+            size_t capacity;
+            char* ptr;
+        } heap;
+        char sso[sizeof(struct large_string) - 1];
+    } data;
+    size_t len;
+    bool on_heap;
+};
+
+int string_buffer_init(struct string_buffer* sb);
+void string_buffer_destroy(struct string_buffer* sb);
+int string_buffer_append(struct string_buffer* sb, const char* str, size_t len);
+int string_buffer_append_cstr(struct string_buffer* sb, const char* str);
+const char* string_buffer_get_cstr(const struct string_buffer* sb);
+size_t string_buffer_get_len(const struct string_buffer* sb);
+
+#endif
