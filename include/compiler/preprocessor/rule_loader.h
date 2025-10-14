e45248a3d864bcc12b239a7ebde43430522d35c2
matee8 <graves-bluff-pesky@duck.com>
2025-10-14 11:08:22 +0200

    refactor!: change language to Rust

--- include/compiler/preprocessor/rule_loader.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMPILER_PREPROCESSOR_RULE_LOADER_H
-#define COMPILER_PREPROCESSOR_RULE_LOADER_H
-
-#include "compiler/ds/vector.h"
-
-int rule_loader_load_from_file(const char* filename, struct vector* rules);
-void rule_loader_free_rules(struct vector* rules);
-
-#endif
