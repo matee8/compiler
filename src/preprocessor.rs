b98392130fd088984571c00ebda699cae80adbf5
matee8 <graves-bluff-pesky@duck.com>
2025-10-14 18:58:19 +0200

    feat(preprocessor): implement rule parser

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,1 +1,37 @@
+use rule::{InvalidRegex, Rule};
+use thiserror::Error;
+
 pub mod rule;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum ParseError<'src> {
+    #[error("{0}")]
+    InvalidRegex(InvalidRegex<'src>),
+    #[error("malformed rule on line {line_number}: '{line_content}'")]
+    MalformedRule {
+        line_number: usize,
+        line_content: &'src str,
+    },
+}
+
+#[inline]
+pub fn parse_rules(content: &str) -> Result<Vec<Rule<'_>>, ParseError<'_>> {
+    content
+        .lines()
+        .enumerate()
+        .map(|(i, line)| (i, line.trim()))
+        .filter(|&(_, line)| !line.is_empty())
+        .map(|(i, line)| {
+            if let Some((pattern, replacement)) = line.rsplit_once(',') {
+                Rule::new(pattern, replacement)
+                    .map_err(ParseError::InvalidRegex)
+            } else {
+                Err(ParseError::MalformedRule {
+                    line_number: i + 1,
+                    line_content: line,
+                })
+            }
+        })
+        .collect()
+}
